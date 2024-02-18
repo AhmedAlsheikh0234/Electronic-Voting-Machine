ad823b05bcbb4cfa0b281f90c42d45d3fdd5aa7f
AhmedAlsheikh0234 <154998275+AhmedAlsheikh0234@users.noreply.github.com>
2024-02-18 21:27:45 +0200

    Button_Driver.h

---
+++ Button_Driver.h
@@ -0,0 +1,16 @@
+/*
+ * Button_Driver.h
+ *
+ * Created: 11/6/2023 4:07:46 PM
+ *  Author: HaNdSyA---
+ */ 
+
+
+#ifndef BUTTON_DRIVER_H_
+#define BUTTON_DRIVER_H_
+void button_init(char port,char pin);
+char button_read(char port,char pin);
+
+
+
+#endif /* BUTTON_DRIVER_H_ */
